5e1d0c61dc82f0d5f508452959a5c5667a2f7650
flores-97 <floresvictor15@gmail.com>
2025-01-15 20:14:12 -0800

    added author, course, purpose.

--- COMP003A.LectureActivity1/Program.cs
+++ COMP003A.LectureActivity1/Program.cs
@@ -1,4 +1,10 @@
-﻿//namespace level
+﻿/*
+ * Author: Victor Flores
+ * Course: COMP03
+ * Faculty: Jonathon Cruz
+ * Purpose: Basic console app
+ */
+//namespace level
 namespace COMP003A.LectureActivity1
 {
     internal class Program
