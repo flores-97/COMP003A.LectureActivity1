e26b49f8056bd6e470f6bdf8b6ac9c46a3015191
flores-97 <floresvictor15@gmail.com>
2025-01-15 19:59:55 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,12 @@
+﻿//namespace level
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        //class level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
